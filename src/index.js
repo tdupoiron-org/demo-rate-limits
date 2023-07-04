0904a1bfd1582f25d829cb49f64294dcb0fbf872
Thomas Dupoiron <tdupoiron@github.com>
2023-07-04 15:32:45 +0000

    add local action

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 //Use github rest api to get the list of repositories for user tdupoiron
 const { Octokit } = require("@octokit/core");
+const core = require('@actions/core');
+
+const NbLoops = core.getInput('NB_LOOPS');
+const IntervalMilli = core.getInput('INTERVAL_MILLI');
 
 async function getRepositories() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  octokit.request('GET /users/{username}/repos', {
+  await octokit.request('GET /users/{username}/repos', {
     username: 'tdupoiron'
   }).then(({ data }) => {
     console.log(data);
@@ -11,4 +15,20 @@ async function getRepositories() {
   );
 }
 
-getRepositories();
+async function main() {
+  for (let i = 0; i < NbLoops; i++) {
+    console.log(`Loop ${i}`);
+    await getRepositories();
+    sleep(IntervalMilli);
+  } 
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+main();
